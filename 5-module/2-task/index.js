a03c0e1eb08243b4872c1ee96963e4a8e0ddf2bf
wewewew <wewee@weew.rr>
2021-06-09 17:52:07 +0300

    task5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector( '.toggle-text-button' );
+
+  if( button ) {
+    button.addEventListener( 'click', ()=>{
+      let text = document.getElementById( 'text' );
+
+      if( text ) {
+        text.hidden = !text.hidden;
+      }
+    });
+  }
 }
